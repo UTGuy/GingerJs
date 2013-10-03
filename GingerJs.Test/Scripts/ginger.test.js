3dc605572303283d5ffa6b05ba63a541b8ca502e
Mike Walters <mikel.walters@gmail.com>
2013-10-03 16:44:58 -0400

    fixed bug in mapper

--- GingerJs.Test/Scripts/ginger.test.js
+++ GingerJs.Test/Scripts/ginger.test.js
@@ -27,7 +27,7 @@
     });
 
     q.test("View Model is instanceof MyClassModel", function() {
-        
+
         function MyClassModel() {
             this.Id = ko.observable('');
         }
@@ -37,14 +37,15 @@
 
         q.equal(vm instanceof MyClassModel, true);
     });
-    
-    q.test("View Model is instanceof MySubClassModel", function () {
+
+    q.test("View Model is instanceof MySubClassModel", function() {
 
         function MySubClassModel() {
             this.Id = ko.observable('');
         }
+
         var mySubClass = app.bindModel(MySubClassModel);
-        
+
         function MyClassModel() {
             this.Name = ko.observable('');
         }
@@ -53,6 +54,49 @@
         var vm = new myClass();
 
         q.equal(vm instanceof MySubClassModel, true);
+    });
+
+    q.test("2 Different Models in a Map", function() {
+
+        function ParentModel() {
+            this.Child1 = ko.observable();
+            this.Child2 = ko.observable();
+        }
+
+        function FirstSubModel() {
+            this.Ida = ko.observable();
+        }
+
+        function SecondSubModel() {
+            this.Idb = ko.observable();
+        }
+
+        var firstSub = app.bindModel(FirstSubModel);
+        var secondSub = app.bindModel(SecondSubModel);
+
+        function parentMap() {
+            return new app.Map({
+                "Child1": firstSub,
+                "Child2": secondSub
+            });
+        }
+
+        var parent = app.bindModel(ParentModel, parentMap);
+        var vm = new parent({
+            "Child1": {},
+            "Child2": {}
+        });
+
+        var child1 = vm.Child1();
+        var child2 = vm.Child2();
+
+        q.equal(typeof child1 != "undefined", true);
+        q.equal(typeof child2 != "undefined", true);
+
+        var id1 = (null == child1) ? null : child1.Ida();
+        var id2 = (null == child2) ? null : child2.Idb();
+        q.equal(typeof id1 == "undefined", true);
+        q.equal(typeof id2 == "undefined", true);
     });
 
 })(QUnit, Ginger, ko);
--- GingerJs.Web/Scripts/ginger.mapping.js
+++ GingerJs.Web/Scripts/ginger.mapping.js
@@ -11,17 +11,21 @@
             return self[prop];
         }
 
-        function setFn(prop, name, fn) {
-            getProp(prop)[name] = fn;
+        function setFn(model, prop, name) {
+            getProp(prop)[name] = function (options) {
+                return new model(options.data);
+            };
+        }
+
+        function addPropMap(prop, map, type) {
+            var propModel = createMap[createProp];
+            if (typeof propModel == "undefined")
+                throw "'" + createProp + "': map is undefined";
+            setFn(propModel, createProp, type);
         }
 
         for (var createProp in createMap) {
-            var model = createMap[createProp];
-            if (typeof model == "undefined")
-                throw "'" + createProp + "': map is undefined";
-            setFn(createProp, 'create', function(options) {
-                return new model(options.data);
-            });
+            addPropMap(createProp, createMap, 'create');
         }
     }
 
